25fde8139c1ca98061f9a1c7cf8e13e6bc1707e8
Satvik Kumar Jain <satvik.jain@advantawitty.com>
2024-08-02 14:33:07 -0400

    1) Enhanced Nav Bar 2) Added Team Logo

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -6,16 +6,17 @@ const Navbar = () => {
   const { isAuthenticated, signout } = useAuth();
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+
+    <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src="/path/to/your/logo.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top" />
+        <a className="navbar-brand d-flex justify-content-center align-items-center"  href="/">
+          <img src="/MERN Mavericks.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
           MERN Mavericks
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse d-flex justify-content-end align-items-center" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/my-profile">MY PROFILE</Link>
