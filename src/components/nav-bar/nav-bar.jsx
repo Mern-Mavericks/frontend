58fb5970e6b3c507e84a0cfa5e94b3cdb79db297
Satvik Kumar Jain <satvik.jain@advantawitty.com>
2024-08-10 00:44:54 -0400

    Adjust image path for deployment

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           to={isAuthenticated ? '/dashboard' : '/'}
         >
           <img
-            src="src/images/MERN Mavericks.png"
+            src="/src/images/MERN Mavericks.png"
             alt="Logo"
             width="30"
             height="30"
